407ca3ec9633b0c8e639c74d0161cfec4ef989e7
«A»Anastasia <anastassun12@gmail.com>
2022-02-26 17:58:52 +0300

    add TOKEN in bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,20 @@
 from multiprocessing import context
 from turtle import update
 from telegram.ext import Updater, CommandHandler
+
+from token import TOKEN
 def greet_user(update,context):
     print('Вызван /start')
     update.message.reply_text('Привет, пользователь! Ты вызвал команду /start')
 
 
 def main():
-    mybot = Updater("5262680588:AAHXaXg_gIpLk0ANWwmtKg5xbwCgD2Y0koM", use_context=True)
+    mybot = Updater(TOKEN, use_context=True)
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
     mybot.start_polling()
     print("bot_online")
     mybot.idle()
 
-main()
-
-
+main(
     
