9061c29b5c6d6a21f52e27c83931888236a0a935
Ivan Grubov <qalbertooq@mail.ru>
2022-03-24 19:54:07 +0300

    Удалил лишний код

--- forma_add_to_db.py
+++ forma_add_to_db.py
@@ -13,31 +13,6 @@ def name_vacan_and_company():
     else:
         return vacancy, company
 
-def slot_quest():
-    blank_forms = input("Введите количество вопросов: ")
-    check_res = slot_in_company(blank_forms)
-    if check_res == "+":
-        return slot()
-    else:
-        return int(blank_forms)
-
-def slot():
-    blank_forms = input("Введите количество вакансий для этой компании: ")
-    check_res = slot_in_company(blank_forms)
-    if check_res == "+":
-        return slot()
-    else:
-        return int(blank_forms)
-
-def slot_in_company(num):
-    if num:
-        try:
-            good_num = abs(int(num))
-        except (TypeError, ValueError):
-            print("Введите целое число")
-            return "+"
-        return '-'
-
 def info_job():
     key = input('Введите код документа: ')
     try:
@@ -51,5 +26,4 @@ def info_job():
 
 if __name__ == '__main__':
     file = info_job()
-    print(file)
-    job = get_or_create_job(db, file)
+    get_or_create_job(db, file)
