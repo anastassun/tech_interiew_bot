8629cb97acc860b8ee1e58296ebe5043dae5a479
anastassun <99546747+anastassun@users.noreply.github.com>
2022-04-12 16:03:38 +0300

    добавила таймер в анкету

--- anketa.py
+++ anketa.py
@@ -2,7 +2,7 @@ from telegram import ReplyKeyboardRemove, ReplyKeyboardMarkup
 from telegram.ext import ConversationHandler
 from db import db, info_vacan_in_company, user_profile
 from utils import del_use_num, new_button, format_dict, check_phone, main_keyboard
-from format_word_anketa_user import create_word_file
+#from format_word_anketa_user import create_word_file
 
 NAME, DISCUSSION, QUESTION, NUMQUESTION, CONTACT, FINAL = range(6)
 
@@ -20,6 +20,7 @@ def name(update,context):
         update.message.reply_text("Пожалуйста введите имя и фамилию")
         return NAME
     else:
+        print(context.user_data["vacan"])
         message = info_vacan_in_company(db, context.user_data["vacan"])
         context.user_data["name"] = user_name
         reply_keyboard = [[]]
@@ -62,15 +63,26 @@ def quest(update, context):
         context.user_data['num_answer'] = [str(sms)]
         return NUMQUESTION
 
+def alarm(context):
+    context.bot.send_message(chat_id=context.job.context["chat_id"], text="у вас осталось 10 секунд на ответ!")
+
 def num_qestion(update, context):
     num_answer = context.user_data['num_answer']
     context.user_data[num_answer[0]] = update.message.text
     num = context.user_data['question']
     old_num = del_use_num(context.user_data)
     new_buttons = new_button(num, old_num)
+    
+    
     reply_keyboard = [[]]
     reply_keyboard.append(new_buttons)
     if new_buttons: #Проверяет есть ли еще не нажатые кнопки, если кнопки есть True, если нету False.
+        job_context={
+        "chat_id":update.message.chat_id,
+    }
+        context.job_queue.scheduler.remove_all_jobs()
+        context.job_queue.run_once(alarm, 10, context=job_context)
+
         update.message.reply_text('Выберите следующий вопрос',reply_markup=ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True))
         return QUESTION
     else:
@@ -92,7 +104,7 @@ def contact(update, context):
 def final(update, context): 
     try:
         userprofile = format_dict(context.user_data)
-        create_word_file(userprofile)
+        #create_word_file(userprofile)
     except (FileNotFoundError, KeyError, AttributeError, TypeError) as err:
         update.message.reply_text(f'Возникла ошибка {err}', reply_markup=main_keyboard())
         return ConversationHandler.END
